8bf7d3db77d6a883cd9a889048eb3a54f08423a5
NamVo <51428674+namvotech@users.noreply.github.com>
2025-01-09 23:01:33 +0700

    first commit

--- main.ts
+++ main.ts
@@ -1,1 +1,13 @@
-
+pins.digitalWritePin(DigitalPin.P13, 0)
+pins.digitalWritePin(DigitalPin.P14, 0)
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A) || input.buttonIsPressed(Button.B)) {
+        pins.digitalWritePin(DigitalPin.P13, 0)
+        pins.digitalWritePin(DigitalPin.P14, 1)
+        basic.showIcon(IconNames.Heart)
+    } else {
+        pins.digitalWritePin(DigitalPin.P13, 0)
+        pins.digitalWritePin(DigitalPin.P14, 0)
+        basic.showIcon(IconNames.SmallHeart)
+    }
+})
